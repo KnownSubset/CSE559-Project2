88c228cd24fc74cb2daac1f93f31f80dcb320c32
Nathan Dauber <Nathan.Dauber@gmail.com>
2012-02-27 13:10:20 -0600

    working on the calculating the homography matrix

--- Mosaicing.m
+++ Mosaicing.m
@@ -1,9 +1,9 @@
 run('vlfeat-0.9.14/toolbox/vl_setup');
 imBlur = imfilter(imread('/Users/nathan/Development/CSE559-Project2/left1.png'),fspecial('Gaussian',[5 5],1));
-LeftImage = im2double(imBlur)/255;
+LeftImage = im2double(imresize(imBlur,[400,525]))/255;
 LeftImage = rgb2gray(LeftImage);
 imBlur = imfilter(imread('/Users/nathan/Development/CSE559-Project2/right1.png'),fspecial('Gaussian',[5 5],1));
-RightImage = im2double(imBlur)/255;
+RightImage = im2double(imresize(imBlur,[400,525]))/255;
 RightImage = rgb2gray(RightImage);
 [F_Left,D_Left] = vl_sift(im2single(LeftImage));
 [F_Right,D_Right] = vl_sift(im2single(RightImage));
@@ -39,4 +39,30 @@ for ix = 1:descriptorCount;
     end
     [rte, index] = min(similarity(ix,:));
     maxSimilarity(1,ix) = index;
+end
+
+[matches,scores] = vl_ubcmatch(D_Left, D_Right);
+
+hscores = zeros(1,1:40);
+for ih = 1:40
+    points = randi(size(matches,2),1,4);
+    A = [];
+    b = [];
+    for ip = 1:4
+        point1 = matches(1,ip);
+        point2 = matches(2,ip);
+        p1_X = F_Left(1,point1);
+        p1_Y = F_Left(2,point1);
+        p2_X = F_Right(1,point2);
+        p2_Y = F_Right(2,point2);
+        A = cat(1, A, [-p1_X -p1_Y  1     0     0  0 -p1_X*p2_X -p1_Y*p2_Y]);
+        A = cat(1, A, [    0     0  0 -p1_X -p1_Y  1 -p1_X*p2_X -p1_Y*p2_Y]);
+        b = cat(1, b,[-p2_X; -p2_Y]);
+     
+    end    
+    H = A/b;
+    
+    tLeft = H.*LeftImage;
+    
+    hscores(ih) = 0;
 end
